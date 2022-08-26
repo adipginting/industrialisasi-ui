ccd8faace579dc2cc0100bd77b7ac3c2b43cc011
adipginting <adi@adipginting.com>
2022-08-26 20:22:12 +0700

    Supplying code for button onClick event

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+const axios = require('axios').default;
+
 
 const App = () => {
+  const {email, setEmail} = useState('');
+  const postEmail = (async (event) => {
+    try{
+      const result = await axios.post('http://localhost:4000', email);
+      console.log(result);
+    }
+    catch(error){
+      console.error(error);
+    }
+    event.preventDefault();
+  })();
   return (
     <Container fluid='lg'>
       <Row>
@@ -14,7 +28,12 @@ const App = () => {
             <div className='mt-2 mb-2'><b>Enter your email, username, and password to register.</b></div>
             <Form.Group className='mb-2'>
               <Form.Label> Email: </Form.Label>
-              <Form.Control type='email' placeholder='email'></Form.Control>
+              <Form.Control
+                type='email'
+                placeholder='email'
+                onChange = {event => {setEmail(event.target.value)}}
+              >
+            </Form.Control>
             </Form.Group>
             <Form.Group className='mb-2'>
               <Form.Label>Username: </Form.Label>
@@ -25,7 +44,7 @@ const App = () => {
               <Form.Control type='password' placeholder='Password'></Form.Control>
             </Form.Group>
           </Form>
-          <Button type='send'>Send</Button>
+          <Button type='send' onClick={event => postEmail(event)}>Send</Button>
         </Col>
         <Col></Col>
         <Col></Col>
