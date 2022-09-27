9fab1027953119af19be65d0b45ada3f1af59d4c
adipginting <adi@adipginting.com>
2022-09-27 12:09:52 +0700

    Registration page is 90% done

--- src/Register.js
+++ src/Register.js
@@ -10,13 +10,16 @@ import validator from "validator";
 import PasswordStrengthIndicator from "react-password-strength-bar";
 
 const axios = require("axios");
-
 const Register = () => {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [passwordRepeat, setPasswordRepeat] = useState("");
+  const [verifier, setVerifier] = useState("");
+  const [isVerifierCodeValid, setVerifierCodeValidy] = useState(false);
+  const [hasVerifierCodeTimedout, setVerifierCodeTimedout] = useState(false);
   const [isEmailValid, setEmailValidity] = useState(false);
+  const [isEmailSent, setEmailSent] = useState(false);
   const [doesEmailExistsonDb, setEmailExistenceOnDb] = useState(false);
   const [isUsernameValid, setUsernameValidity] = useState(false);
   const [doesUsernameExistsonDb, setUsernameExistenceOnDb] = useState(false);
@@ -52,7 +55,6 @@ const Register = () => {
     };
   }, [email, setEmailValidity, setEmailExistenceOnDb]);
 
-  //fetch username
   useEffect(() => {
     const checkUsername = async (_username_) => {
       try {
@@ -67,7 +69,7 @@ const Register = () => {
           } else {
             setUsernameExistenceOnDb(false);
           }
-        } else if (validator.isAlphanumeric(_username_) === false){
+        } else if (validator.isAlphanumeric(_username_) === false) {
           setUsernameValidity(false);
         }
       } catch (error) {
@@ -83,13 +85,17 @@ const Register = () => {
     };
   }, [username, setUsernameValidity, setUsernameExistenceOnDb]);
 
-  const postLoginInfo = async (_loginInfo_) => {
-    try {
-      await axios.post("http://localhost:4000/register", _loginInfo_);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      if (isEmailSent === true){
+        setVerifierCodeTimedout(true);
+      }
+    }, 30000);
+
+    return () => {
+      clearTimeout(delay);
+    };
+  }, [isEmailSent, setVerifierCodeTimedout]);
 
   const emailHandler = (event) => {
     setEmail(event.target.value);
@@ -99,7 +105,7 @@ const Register = () => {
   const usernameHandler = (event) => {
     setUsername(event.target.value);
     event.preventDefault();
-  }
+  };
 
   const passwordHandler = (event) => {
     setPassword(event.target.value);
@@ -111,19 +117,30 @@ const Register = () => {
     event.preventDefault();
   };
 
+  const verifierHandler = (event) => {
+    setVerifier(event.target.value);
+    event.preventDefault();
+  };
+
   const isPasswordValid = () => {
-    if (password.length > 6)
-      return true;
+    if (password.length > 6) return true;
     return false;
   };
 
   const isPasswordRepeatValid = () => {
-    if (passwordRepeat.length > 6)
-      return true;
+    if (passwordRepeat.length > 6) return true;
     return false;
   };
 
   const onSubmitHandler = (event) => {
+    const postLoginInfo = async (_loginInfo_) => {
+      try {
+        await axios.post("http://localhost:4000/register", _loginInfo_);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
     const onSubmit = async () => {
       if (
         email === "" ||
@@ -133,7 +150,7 @@ const Register = () => {
       ) {
         alert("Please fill all of the fields before submitting.");
         event.preventDefault();
-      } else if (areRegistrationFieldsValid()){
+      } else if (areRegistrationFieldsValid()) {
         const loginInfo = {
           email: email,
           username: username,
@@ -151,7 +168,7 @@ const Register = () => {
         alert("Please conform to the warnings and errors before submitting.");
         event.preventDefault();
       }
-    }
+    };
 
     onSubmit(); //call onsubmit function which is an async function.
   };
@@ -170,8 +187,48 @@ const Register = () => {
       isPasswordValid() &&
       isPasswordRepeatValid() &&
       isPasswordRepeated()
-    ) return true;
+    )
+      return true;
     return false;
+  };
+
+  const emailSubmitHandler = (event) => {
+    const sendVerificationCodeEmail = async () => {
+      const { data } = await axios.post("http://localhost:4000/sendemail", {
+        email: email,
+      });
+
+      console.log("Was email send? " + data);
+
+      if (isEmailValid && doesEmailExistsonDb === false) {
+        if (data === true) {
+          setEmailSent(true);
+        } else {
+          setEmailSent(false);
+        }
+      }
+    };
+    sendVerificationCodeEmail();
+    event.preventDefault();
+  };
+
+  const submitVerifierCode = (event) => {
+    const checkVerifierCode = async () => {
+      if (validator.isAlphanumeric(verifier) && hasVerifierCodeTimedout === false) {
+        const { data } = await axios.post("http://localhost:4000/verifier", {
+          email: email,
+          code: verifier,
+        });
+        console.log("Is verifier valid? " + data);
+        if (data === true) {
+          setVerifierCodeValidy(true);
+        } else {
+          setVerifierCodeValidy(false);
+        }
+      }
+    };
+    checkVerifierCode();
+    event.preventDefault();
   };
 
   return (
@@ -179,113 +236,170 @@ const Register = () => {
       <Row>
         <Col md></Col>
         <Col sm md>
-          <Form onSubmit={onSubmitHandler}>
-            <div className="mt-2 mb-2">
-              <b>Enter your email, username, and password to register.</b>
-            </div>
-            <Form.Group className="mb-2">
-              <Form.Label> Email*: </Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="email"
-                onChange={emailHandler}
-                value={email}
-              ></Form.Control>
-              {isEmailValid === false && email !== "" && (
-                <div className="assist">⚠️ Please use a real email. </div>
-              )}
-              {doesEmailExistsonDb && (
-                <div className="assist">
-                  ❌ This email has been registered. Please use another email.
-                </div>
-              )}
-              {doesEmailExistsonDb === false && isEmailValid && (
-                <div className="go">✔️ Email looks good. </div>
-              )}
-            </Form.Group>
-            <Form.Group className="mb-2">
-              <Form.Label>Username*: </Form.Label>
-              <Form.Control
-                type="username"
-                placeholder="username"
-                onChange={usernameHandler}
-                value={username}
-              ></Form.Control>
-               { isUsernameValid === false && username !== "" && (
+          {isEmailSent === false && (
+            <Form onSubmit={emailSubmitHandler}>
+              <p className="mt-2">
+                <strong>Registering to industrialisasi. </strong>
+              </p>
+              <div className="mb-2">
+                <strong>Please enter your email. </strong>
+              </div>
+              <Form.Group className="mb-2">
+                <Form.Label> Email: </Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="email"
+                  onChange={emailHandler}
+                  value={email}
+                ></Form.Control>
+                {isEmailValid === false && email !== "" && (
+                  <div className="assist">⚠️ Please use a real email. </div>
+                )}
+                {doesEmailExistsonDb && (
                   <div className="assist">
-                    ⚠️  Username has to be consisted of only alphabets and numbers.
+                    ❌ This email has been registered. Please use another email.
                   </div>
                 )}
-              { username.length <= 3 && username !== "" && (
+                {doesEmailExistsonDb === false && isEmailValid && (
+                  <div className="go">✔️ Email looks good. </div>
+                )}
+                <Button type="submit" className="mt-2">
+                  Submit Email
+                </Button>
+              </Form.Group>
+            </Form>
+          )}
+          {isEmailSent === true && isVerifierCodeValid === false && (
+            <Form onSubmit={submitVerifierCode}>
+              <p className="mt-2">
+                <strong>Registering to industrialisasi. </strong>
+              </p>
+              <div className="mb-2">
+                <strong>
+                  Please check <span className="go">{email}'s </span> inbox/spam
+                  folder, a six character code has been sent to your email. {" "}
+                </strong>
+              </div>
+              <Form.Group className="mb-2">
+                <Form.Label> Six character verifier code: </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="verifier code"
+                  onChange={verifierHandler}
+                  value={verifier}
+                ></Form.Control>
+                {isVerifierCodeValid === false &&
+                  verifier !== "" &&
+                  verifier.length < 6 && (
+                    <div className="assist">
+                      ⚠️ The Verifier code consists of six character of numbers and letters.{" "}
+                    </div>
+                  )}
+                {
+                  hasVerifierCodeTimedout === true &&
                   <div className="assist">
-                    ⚠️  Username has to be more than 3
-                    characters long.
+                    ❌ Timeout. The code is no longer valid. Refresh the page to try again.
+                  </div>
+                }
+
+                <Button type="submit" className="mt-2">
+                  Submit code
+                </Button>
+              </Form.Group>
+           </Form>
+          )}
+          {isVerifierCodeValid === true && (
+            <Form onSubmit={onSubmitHandler}>
+              <p className="mt-2">
+                <strong>Registering to industrialisasi. </strong>
+              </p>
+              <div className="mb-2">
+                <strong>Please enter your username and password. </strong>
+              </div>
+              <Form.Group className="mb-2">
+                <Form.Label>Username*: </Form.Label>
+                <Form.Control
+                  type="username"
+                  placeholder="username"
+                  onChange={usernameHandler}
+                  value={username}
+                ></Form.Control>
+                {isUsernameValid === false && username !== "" && (
+                  <div className="assist">
+                    ⚠️ Username has to be consisted of only alphabets and
+                    numbers.
+                  </div>
+                )}
+                {username.length <= 3 && username !== "" && (
+                  <div className="assist">
+                    ⚠️ Username has to be more than 3 characters long.
                   </div>
                 )}
 
-              {doesUsernameExistsonDb && (
-                <div className="assist">
-                  ❌: Unfortunately, the username had been taken. Please, choose
-                  another username.
-                </div>
-              )}
-              {doesUsernameExistsonDb === false && isUsernameValid && (
-                <div className="go">✔️ Username looks good. </div>
-              )}
-            </Form.Group>
-            <Form.Group className="mb-2">
-              <Form.Label> Password*: </Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                onChange={passwordHandler}
-                value={password}
-              ></Form.Control>
-              {isPasswordValid() && (
-                <PasswordStrengthIndicator password={password} />
-              )}
-              {isPasswordValid() === false && password !== "" && (
-                <div className="assist">
-                  ⚠️ Password must be more than six characters.
-                </div>
-              )}
-            </Form.Group>
-            <Form.Group className="mb-2">
-              <Form.Label> Repeat password*: </Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Repeat password"
-                onChange={passwordRepeatHandler}
-                value={passwordRepeat}
-              ></Form.Control>
-              {isPasswordRepeatValid() && (
-                <PasswordStrengthIndicator
-                  password={passwordRepeat}
-                />
-              )}
-              {isPasswordRepeatValid() === false && passwordRepeat !== "" && (
-                <div className="assist">
-                  ⚠️ Password must be more than six characters.
-                </div>
-              )}
-              {isPasswordRepeated() === false && passwordRepeat !== "" && (
-                <div className="assist">⚠️ Passwords don't match. </div>
-              )}
-              {isPasswordRepeatValid() && isPasswordValid() && isPasswordRepeated() === true && passwordRepeat !== "" && (
-                <div className="go">✔️ Passwords match!</div>
-              )}
-            </Form.Group>
-            <Form.Label>
-              <div>*) Required.</div>
-            </Form.Label>
-            <br />
-            <Button type="submit">Submit</Button>
-          </Form>
+                {doesUsernameExistsonDb && (
+                  <div className="assist">
+                    ❌: Unfortunately, the username had been taken. Please,
+                    choose another username.
+                  </div>
+                )}
+                {doesUsernameExistsonDb === false && isUsernameValid && (
+                  <div className="go">✔️ Username looks good. </div>
+                )}
+              </Form.Group>
+              <Form.Group className="mb-2">
+                <Form.Label> Password*: </Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  onChange={passwordHandler}
+                  value={password}
+                ></Form.Control>
+                {isPasswordValid() && (
+                  <PasswordStrengthIndicator password={password} />
+                )}
+                {isPasswordValid() === false && password !== "" && (
+                  <div className="assist">
+                    ⚠️  Password must be more than six characters.
+                  </div>
+                )}
+              </Form.Group>
+              <Form.Group className="mb-2">
+                <Form.Label> Repeat password*: </Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Repeat password"
+                  onChange={passwordRepeatHandler}
+                  value={passwordRepeat}
+                ></Form.Control>
+                {isPasswordRepeatValid() && (
+                  <PasswordStrengthIndicator password={passwordRepeat} />
+                )}
+                {isPasswordRepeatValid() === false && passwordRepeat !== "" && (
+                  <div className="assist">
+                    ⚠️ Password must be more than six characters.
+                  </div>
+                )}
+                {isPasswordRepeated() === false && passwordRepeat !== "" && (
+                  <div className="assist">⚠️ Passwords don't match. </div>
+                )}
+                {isPasswordRepeatValid() &&
+                  isPasswordValid() &&
+                  isPasswordRepeated() === true &&
+                  passwordRepeat !== "" && (
+                    <div className="go">✔️ Passwords match!</div>
+                  )}
+              </Form.Group>
+              <Form.Label>
+                <div>*) Required.</div>
+              </Form.Label>
+              <br />
+              <Button type="submit">Create Account</Button>
+            </Form>
+          )}
         </Col>
         <Col sm md></Col>
       </Row>
     </Container>
   );
 };
-
 export default Register;
