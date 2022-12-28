2fdde50075a757b4c0ef6becb57df183c26a40c8
adipginting <adi@adipginting.com>
2022-12-28 19:29:48 +0700

    cleaning up

--- src/api/api.js
+++ src/api/api.js
@@ -1,21 +1,9 @@
 import axios from 'axios';
 
-const auth = () => {
-  if (localStorage.getItem("jwttoken") === null)
-    return 'no token';
-  else if (typeof(localStorage.getItem("jwttoken")) === 'undefined')
-    return 'undefined';
-  else if (localStorage.getItem("jwttoken") === 'loggedout')
-    return 'loggedout';
-  else
-    return "Bearer " + localStorage.getItem("jwttoken");
-};
-
 const api = axios.create({
   baseURL: 'http://localhost:4000',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': auth(),
   }
 });
 
