08c1fccb93e0bdbcf4be8ff7eef7bf8576a2cf81
adipginting <apg.foss@gmail.com>
2023-07-02 14:24:03 +0700

    fix:api url

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.industrialisasi.site",
+  baseURL: "https://api.industrialisasi.site/blog",
   headers: {
     "Content-Type": "application/json",
   },
