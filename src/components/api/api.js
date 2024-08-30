4c207a7f08dd9efb0ac9979f0e8dfd82a162019e
adipginting <apg.foss@gmail.com>
2024-08-30 22:33:43 +0700

    ffix: change api to api.industrialisasi.com

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.industrialisasi.com/blog",
+  baseURL: "https://api.industrialisasi.com",
   headers: {
     "Content-Type": "application/json",
   },
