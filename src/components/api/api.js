573d6dc438e23ae224d135349d3e9cacc80a9246
adipginting <apg.foss@gmail.com>
2025-01-05 18:42:02 +0700

    feat: change api base url

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://api.industrialisasi.com/blog",
   headers: {
     "Content-Type": "application/json",
   },
