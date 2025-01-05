83b83bfd83ab7d597b6eca8fd62f15215760463c
adipginting <apg.foss@gmail.com>
2025-01-05 17:59:59 +0700

    feat: use local api

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.industrialisasi.com/blog",
+  baseURL: "http://localhost:4000",
   headers: {
     "Content-Type": "application/json",
   },
